dc4c90a8a54fcc4e194ec3b347d137cb5fe48827
Belal <119034702+BelalKhaleel@users.noreply.github.com>
2023-07-20 15:21:57 +0300

    Create migration files and establish their relationships

---
+++ example-app/app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+    ];
+
+    /**
+     * Get the comments for the blog post.
+     */
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    /**
+     * Get the user that owns the post.
+     */
+    public function user(): BelongsTo 
+    {
+        return $this->belongsTo(User::class);
+    }
+}
