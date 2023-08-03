8bef94241ed8d760a758edecf45fb7494d2ae023
Belal <119034702+BelalKhaleel@users.noreply.github.com>
2023-08-03 22:44:30 +0300

    Attempt to authenticate users (so far not successful)

--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -21,6 +21,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+ 
+    return ['token' => $token->plainTextToken];
+});
+
 Route::apiResource('users', UserController::class);
-Route::apiResource('posts', PostController::class);
+Route::apiResource('posts', PostController::class)->middleware('auth:sanctum');
 Route::apiResource('comments', CommentController::class);
